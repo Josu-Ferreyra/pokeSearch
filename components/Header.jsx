02eefc6d4d92d69b28064026804db5c85430b893
Josue Ferreyra <josueferreyra8824@gmail.com>
2023-01-19 21:29:11 -0300

    CHANGED: Removed home link and made PokeSearch logo the home link

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,12 @@ import styles from '../styles/Header.module.css'
 export function Header () {
   return (
     <header className={styles.headerContainer}>
-      <h1 className={styles.logo}>Pokesearch</h1>
-      <nav className={styles.navbar}>
+      <Link href='/' className={styles.logo}>Pokesearch</Link>
+      {/* <nav className={styles.navbar}>
         <ul>
           <li><Link href='/'>Home</Link></li>
         </ul>
-      </nav>
+      </nav> */}
     </header>
   )
 }
